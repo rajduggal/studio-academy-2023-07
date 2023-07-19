7ccdc75986cc03735ea2daf5769103d5897ec534
Debaprasad Sahoo <debasahoo@deloitte.com>
2023-07-19 14:06:45 +0530

    [lazy]: lazy laoding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect, createContext, useContext } from "react";
-import logo from "./logo.svg";
+import React, { createContext, lazy, Suspense } from "react";
 import "./App.scss";
-import { navItems, useData, listOfCourses } from "./constants";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import FAQ from "./pages/FAQ";
-import CoursesList from "./pages/CoursesList";
-import TrainersList from "./pages/TrainersList";
 import NoPage from "./pages/NoPage";
+
+const Home = lazy(() => import("./pages/Home"));
+const FAQ = lazy(() => import("./pages/FAQ"));
+const CoursesList = lazy(() => import("./pages/CoursesList"));
+const TrainersList = lazy(() => import("./pages/TrainersList"));
 
 const ContextData = createContext();
 
@@ -17,15 +16,17 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="courses" element={<CoursesList />} />
-          <Route path="trainers" element={<TrainersList />} />
-          <Route path="faq" element={<FAQ />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading....</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="courses" element={<CoursesList />} />
+            <Route path="trainers" element={<TrainersList />} />
+            <Route path="faq" element={<FAQ />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
